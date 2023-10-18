427a526005e56f0f890e21d92a42cf7d2cac02dc
Ariela88 <135212709+Ariela88@users.noreply.github.com>
2023-10-18 17:06:51 +0200

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './material/material.module';
-
+import { HeaderComponent } from './components/header/header.component';
+import { BookEditorComponent } from './components/book-editor/book-editor.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { CardBookComponent } from './components/card-book/card-book.component';
+import { FavouritesComponent } from './components/favourites/favourites.component';
+import { HomeLibraryComponent } from './components/home-library/home-library.component';
 import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    BookEditorComponent,
+    BookListComponent,
+    CardBookComponent,
+    FavouritesComponent,
+    HomeLibraryComponent
+    
    
   ],
   imports: [
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { MaterialModule } from 'src/app/material/material.module';
 
 
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [CommonModule,MaterialModule],
+
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
-  constructor(private router: Router) { }
-
+  constructor(private router: Router) {}
 }
